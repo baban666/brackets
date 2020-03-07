f35032d7adfe7b93c5b9c9f052b15f7d29507a93
Vladimir Veraks <vladimirveraksa@gmail.com>
2020-03-07 04:51:54 +0300

    feat: brackets done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let pairsBrackets = [];
+    let result = null;
+
+   bracketsConfig.forEach((item) => {
+       pairsBrackets.push(item.join(''))
+   });
+
+    const  getResult = (pairsBrackets, str, count = 0) => {
+
+        if(str.indexOf(pairsBrackets[count] ) !== -1){
+            str = str.replace(pairsBrackets[count], "");
+            return getResult(pairsBrackets, str, count = 0)
+        }else if(count < pairsBrackets.length){
+            count ++
+            return getResult(pairsBrackets, str, count)
+        }
+
+        return  (str) ? result = false : result = true;
+    }
+    return  getResult(pairsBrackets, str);
 }
+
